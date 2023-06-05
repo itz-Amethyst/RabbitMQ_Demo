83786b4691194099f54494acf67971794c27586c
itz-amethyst <pransermi@gmail.com>
2023-06-05 20:16:12 +0430

    feat: Added Booking class

---
+++ RabbitMQSender/Booking.cs
@@ -0,0 +1,16 @@
+﻿namespace RabbitMQSender
+{
+    public class Booking
+    {
+        public int Id { get; set; } = 1;
+
+        public string PassengerName { get; set; } = "Test";
+
+        public string PassportNumber { get; set; } = "12354678";
+
+        public string From { get; set; } = "NoWhere";
+
+        public string To { get; set; } = "SomeWhere";
+
+    }
+}
