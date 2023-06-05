592398a29decb5ac6d796eb01bc814f384f23297
itz-amethyst <pransermi@gmail.com>
2023-06-05 19:20:53 +0430

    feat: Added Message Sender

--- RabbitMQSender/Program.cs
+++ RabbitMQSender/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+factory.ClientProvidedName = "Rabbit Sender App";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+
+string routingKey = "demo-routing-key";
+
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName , ExchangeType.Direct);
+
+channel.QueueDeclare(queueName , false , false , false  , null);
+
+channel.QueueBind(queueName , exchangeName , routingKey , null);
+
+byte[] messageBody = Encoding.UTF8.GetBytes("Hello World");
+
+channel.BasicPublish(exchangeName , routingKey , null , messageBody);
+
+
+channel.Close();
+cnn.Close();
